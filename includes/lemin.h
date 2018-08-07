e2cd473e282443d4681323d59dcd2f5c6536c043
Richard HOHLS <rhohls@e4r14p8.wethinkcode.co.za>
2018-08-07 14:38:05 +0200

    start headder

--- includes/lemin.h
+++ includes/lemin.h
@@ -6,7 +6,7 @@
 /*   By: rhohls <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 08:57:02 by rhohls            #+#    #+#             */
-/*   Updated: 2018/08/07 08:58:43 by rhohls           ###   ########.fr       */
+/*   Updated: 2018/08/07 10:15:50 by rhohls           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,38 @@
 # define LEMIN_H
 
 # include "../libft/includes/libft.h"
+
+typedef struct	s_room
+{
+	char		*name;
+	int			occupied;
+	int			num_connections;
+	char		**conections;
+}				t_room;
+
+typedef struct	s_path
+{
+	char		**path;
+	int			path_step;
+}				t_path;
+
+// typedef t_stack t_path;
+
+typedef struct	s_ant
+{
+	int			number;
+	t_path		path;
+}				t_ant;
+
+typedef struct	s_lemin
+{
+	char		*start;
+	char		*end;
+	t_room		*room_list;
+	int			num_ants;
+	t_ant		*ant_list;
+}				t_lemin;
+
+
+
 #endif
