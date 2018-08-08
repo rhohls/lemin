4842722cf5639ad1a4303190890924534f421715
Richard H <richard.hohls@gmail.com>
2018-08-08 22:09:40 +0200

    sudo and basic search

--- src/inits.c
+++ src/inits.c
@@ -10,8 +10,140 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void bad(void)
+
+#include "../includes/lemin.h"
+
+typedef struct	s_pathend
 {
-	int a = 1;
-	a++;
+	char		*end;
+	t_stack		*curr_path_list; //unkown type
+	void		all_connections;
+	void		turn_moves; //should prob be stack of stacks
+	
+	char		*room_name;
+	int			turn_num;
+}				t_pathend;
+
+
+int ft_strinstack(char *str, t_stack *stack_x)
+{
+	t_list	*lst;
+	
+	lst = stack_x->start;
+	while (lst)
+	{
+		if (ft_strcmp(str, (char *)(lst->content)) == 0)
+			return (1);
+		lst = lst->next;
+	}
+	return(0);
+}
+
+void free_var(t_pathend *self)
+{
+	/* free all the variables */
+}
+
+t_pathend *duplicate_var(t_pathend *self)
+{
+	/* duplicate all the variables */
+}
+
+char **gen_con_list(char *room_name, void all_connections)
+{
+	char **ret;
+	ret = NULL;
+	return (ret);
+}
+
+void add_room_tolist(char *room_name, void path_list)
+{
+	/* add the room to the path list */
+}
+
+void update_shrtpth( void curr_path_list, t_stack *shortest_path)
+{
+	/* change shortest path to something else */
+}
+
+
+make_new_branch(t_pathend *self, t_stack *shortest_path)
+{
+	char	**room_con_list;
+	t_new	next_room;
+	int i;
+	i = 0;
+	room_con_list = con_list(self->curr_room, self->all_connections);
+	self->turn_num += 1;
+	
+	while (i < len(room_con_list))
+	{
+		self->curr_room = room_con_list[i];
+		if (isnot_occupied(room_con_list[i], self->turnmoves, self->turn_num) && !isinpath(room_con_list[i], self->curr_path_list))
+			path_to_end(duplicate_var(self), shortest_path);
+		i++;
+	}
+	free (room_con_list);
+}
+/*
+** Recuresive function
+** Branches and runs down each branch to find the end
+** updates the shortest path to reflect this.
+*/
+
+void path_to_end(t_pathend *self, t_stack *shortest_path)
+{
+	if (self->curr_path_list->length > shortest_path->length)
+	{
+		delete_var(self);
+		return ();
+	}
+
+	add_room_tolist(self->curr_room, self->curr_path_list);
+	
+	if (ft_strcmp(self->end, self->curr_room) == 0)
+		update_shrtpth(self->curr_path_list, shortest_path)
+	else
+		make_new_branch(self, shortest_path)
+	delete_var(self);
+}
+
+void find_path(turn_moves)
+{
+	t_stack shortest_path;
+	t_pathend self;
+	
+	//init
+	shortest_path.start = NULL;
+	shortest_path.length = 0;
+	init(&self);
+	self->turn_moves = turn_moves;
+	while(shortest_path.start == NULL)
+	{
+		path_to_end(&shortest_path);
+		turn_num++;
+	}
+}
+
+
+void play_game()
+{
+	for each ant
+		ant->path = find_pathturn_moves
+		add ant->path to turn_moves
+
+	print all moves
+}
+
+
+
+
+void main()
+{
+	void	*data;
+
+	data = capture_data();
+	error_check_data(data); //exit inside also use stderr etc.
+	play_game(data);
+	return ;
 }
